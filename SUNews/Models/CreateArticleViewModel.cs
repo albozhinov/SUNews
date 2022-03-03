98e9b15454c8ccc5cd2bad67702f8ba1d1738c4e
albozhinov <a.bozhinov90@gmail.com>
2022-03-03 22:14:39 +0200

    CreateArticle and CreateCategory view models added.

--- SUNews/Models/CreateArticleViewModel.cs
+++ SUNews/Models/CreateArticleViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 using static SUNews.Data.DataConstants;
 
 namespace SUNews.Models
@@ -6,22 +7,26 @@ namespace SUNews.Models
     public class CreateArticleViewModel
     {
         [Required]
-        [StringLength(ArticleContentMaxLength, MinimumLength = ArticleContentMinLength)]
+        [StringLength(ArticleContentMaxLength, MinimumLength = ArticleContentMinLength, ErrorMessage = "Article content must be between {2} and {1}")]
         public string Content { get; init; }
 
         [Required]
-        [StringLength(ArticleTitleMaxLength, MinimumLength = ArticleTitleMinLength)]
+        [StringLength(ArticleTitleMaxLength, MinimumLength = ArticleTitleMinLength, ErrorMessage = "Title must be between {2} and {1}")]
         public string Title { get; init; }
 
         [Required]
-        [StringLength(ArticleImageUrlMaxLength)]
+        [Display(Name = "Image URL")]
+        [StringLength(ArticleImageUrlMaxLength, ErrorMessage = "Image URL must be less than {1} characters.")]
         public string ImageUrl { get; set; }
 
         [Required]
-        [StringLength(AuthorNameMaxLength, MinimumLength = AuthorNameMinLength)]
+        [Display(Name = "Author name")]
+        [StringLength(AuthorNameMaxLength, MinimumLength = AuthorNameMinLength, ErrorMessage = "Author name must be between {2} and {1}")]
         public string AuthorName { get; set; }
 
         public ICollection<string> Categories { get; set; } = new List<string>();
 
+        // This must to be here to list Categories in View!
+        public IEnumerable<SelectListItem>? CategoriesList { get; set; }
     }
 }
---
+++ SUNews/Models/CreateCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SUNews.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using static SUNews.Data.DataConstants;
+
+    public class CreateCategoryViewModel
+    {
+        [Required]
+        [Display(Name = "Category name")]
+        [StringLength(CategoryNameMaxLength, MinimumLength = CategoryNameMinLength, ErrorMessage = "Category name must be between {2} and {1} characters.")]
+        public string CategoryName { get; set; }
+    }
+}
