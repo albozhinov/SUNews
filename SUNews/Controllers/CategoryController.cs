0da55d80f7d7ef85c8fcf45135bd9bf701d62ebb
albozhinov <a.bozhinov90@gmail.com>
2022-03-07 00:25:00 +0200

    CreateCategory and GetAllCategories added.

--- SUNews/Controllers/CategoryController.cs
+++ SUNews/Controllers/CategoryController.cs
@@ -1,17 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SUNews.Services.Contracts;
 
 namespace SUNews.Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService _categoryService)
+        {
+            categoryService = _categoryService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
+        //[Authorize(Roles = "Admin")]
         public IActionResult CreateCategory()
         {
             return View();
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateCategory(string categoryName)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            //Here we have category name and can return to view!
+            var categoryToAdd = await categoryService.CreateCategory(categoryName);
+
+            return Redirect("/Article/CreateArticle");
+        }
+
+
+        public async Task<IActionResult> AllCategories()
+        {
+            var categories = await categoryService.GetAllCategories();
+
+
+            return View(categories);
         }
     }
 }
