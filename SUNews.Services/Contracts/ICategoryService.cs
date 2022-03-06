29a0ae23f03ba7813af26f3473c4c88108de350f
albozhinov <a.bozhinov90@gmail.com>
2022-03-07 00:33:03 +0200

    CategoryService added and implemented GetAllCategories and CreateCategory. Must adding null/empty/whitespace validation and ect.

---
+++ SUNews.Services/Contracts/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using SUNews.Data.Models;
+
+namespace SUNews.Services.Contracts
+{
+    public interface ICategoryService
+    {
+        Task<string> CreateCategory(string categoryName);
+
+        Task<IEnumerable<Category>> GetAllCategories();
+    }
+}
---
+++ SUNews.Services/Services/CategoryService.cs
@@ -0,0 +1,49 @@
+﻿namespace SUNews.Services.Services
+{
+    using Microsoft.EntityFrameworkCore;
+    using SUNews.Data.Models;
+    using SUNews.Data.Repository;
+    using SUNews.Services.Contracts;
+    using System.Collections.Generic;
+
+    public class CategoryService : ICategoryService
+    {
+        private readonly IRepository repository;
+
+        public CategoryService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<string> CreateCategory(string categoryName)
+        {
+            var createdCategoryName = string.Empty;
+
+            var isExists = await repository.All<Category>()
+                .FirstOrDefaultAsync(c => c.Name == categoryName.ToUpper());
+
+            if (isExists != null)
+            {
+                return isExists.Name;
+            }
+
+            var categoryToAdd = new Category()
+            {
+                Name = categoryName.ToUpper(),
+            };
+
+            await repository.AddAsync(categoryToAdd);
+            await repository.SaveAsync();
+
+            return categoryToAdd.Name;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategories()
+        {
+            return await repository.All<Category>()
+                                                .Include(c => c.Articles)
+                                                .ThenInclude(a => a.Article)
+                                                .ToListAsync();
+        }
+    }
+}
