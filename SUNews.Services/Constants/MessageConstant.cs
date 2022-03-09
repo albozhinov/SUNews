ac2f8f5a50c5d35694882b91a58150e3c4b73aba
albozhinov <a.bozhinov90@gmail.com>
2022-03-09 22:52:42 +0200

    MessageConstant added

---
+++ SUNews.Services/Constants/MessageConstant.cs
@@ -0,0 +1,9 @@
+﻿namespace SUNews.Services.Constants
+{
+    public static class MessageConstant
+    {
+        public const string ErrorMessage = "ErrorMessage";
+        public const string SuccessMessage = "SuccessMessage";
+        public const string WarningMessage = "WarningMessage";
+    }
+}
