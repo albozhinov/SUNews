32351f22ea937797103a90e2d22c7431729d5c8d
albozhinov <a.bozhinov90@gmail.com>
2022-03-13 02:10:34 +0200

    Added new functionality

---
+++ SUNews.Services/Providers/IValidatorService.cs
@@ -0,0 +1,11 @@
+﻿namespace SUNews.Services.Providers
+{
+    public interface IValidatorService
+    {
+        public void NullOrWhiteSpacesCheck(string parameter);
+
+        public void NullOrEmptyCollection(ICollection<string> collection);
+
+        public void ValidateModel(object model);
+    }
+}
---
+++ SUNews.Services/Providers/ValidatorService.cs
@@ -0,0 +1,47 @@
+﻿namespace SUNews.Services.Providers
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ValidatorService : IValidatorService
+    {
+        public void NullOrEmptyCollection(ICollection<string> collection)
+        {
+            if (collection == null || collection.Count() == 0 || collection.Contains(String.Empty))
+            {
+                throw new ArgumentNullException("Nullable values are not supported.");
+            }
+
+            return;
+        }
+
+        public void NullOrWhiteSpacesCheck(string parameter)
+        {
+            if (String.IsNullOrEmpty(parameter.ToString()) ||
+                String.IsNullOrWhiteSpace(parameter.ToString()) ||
+                parameter.ToString().Contains(' '))
+            {
+                throw new ArgumentNullException("Nullable or white spaces values are not supported.");
+            }
+
+            return;
+        }
+
+        public void ValidateModel(object model)
+        {
+            var context = new ValidationContext(model);
+            var errorResult = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(model, context, errorResult, true);
+
+            if (isValid)
+            {
+                return;
+            }
+
+            string error = String.Join(", ", errorResult.Select(e => e.ErrorMessage));
+
+            throw new ArgumentException(error);
+        }
+    }
+}
