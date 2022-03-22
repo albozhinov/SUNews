e54323f509bba4289b12cc42760062c67b8f5622
albozhinov <a.bozhinov90@gmail.com>
2022-03-22 19:04:04 +0200

    EditUserAsync added but is not implemented.

--- SUNews.Services/Services/UserService.cs
+++ SUNews.Services/Services/UserService.cs
@@ -1,9 +1,14 @@
 ﻿namespace SUNews.Services.Services
 {
+    using SUNews.Data.Models;
     using SUNews.Services.Contracts;
+    using System.Threading.Tasks;
 
     public class UserService : IUserService
     {
-
+        public Task<User> EditUserAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
