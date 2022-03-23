9f3f80cc368f430f323ef06f58bf9533d20c2082
albozhinov <a.bozhinov90@gmail.com>
2022-03-23 19:13:24 +0200

    CreateRoleViewModel added.

---
+++ SUNews/Areas/Admin/Models/CreateRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SUNews.Areas.Admin.Models
+{
+using System.ComponentModel.DataAnnotations;
+
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        [RegularExpression("^[A-Za-z]+$")]
+        public string Role { get; set; }
+    }
+}
