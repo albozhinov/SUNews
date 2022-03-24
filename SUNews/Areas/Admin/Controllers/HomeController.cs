9fda0b85b9fe5fc1ef7898b65575b09a51d18fb5
albozhinov <a.bozhinov90@gmail.com>
2022-03-24 23:55:10 +0200

    Added Authorize attribute.

--- SUNews/Areas/Admin/Controllers/HomeController.cs
+++ SUNews/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿namespace SUNews.Areas.Admin.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
     [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
     public class HomeController : Controller
     {
         public IActionResult Index()
