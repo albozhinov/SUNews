f97767be78e37098ee6ef5ee9e10a9fd363d17fe
albozhinov <a.bozhinov90@gmail.com>
2022-03-29 00:04:29 +0300

    Added article's details view model.

---
+++ SUNews/Models/DetailsOfArticleViewModel.cs
@@ -0,0 +1,41 @@
+﻿using SUNews.Data.Models;
+
+namespace SUNews.Models
+{
+    public class DetailsOfArticleViewModel
+    {
+        public Guid Id { get; init; }
+
+        public string Content { get; init; }
+
+        public string Title { get; init; }
+
+        public string ImageUrl { get; init; }
+
+        public int Likes { get; init; }
+
+        public IEnumerable<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Guid AuthorId { get; init; }
+
+        public string AuthorName { get; init; }
+
+        public DateTime DateOfCreation { get; init; }
+
+        public IEnumerable<ArticleCategory> Categories { get; set; } = new List<ArticleCategory>();
+
+        public DetailsOfArticleViewModel(Article model)
+        {
+            Id = model.Id;
+            Content = model.Content;
+            Title = model.Title;
+            ImageUrl = model.ImageUrl;
+            Likes = model.LikeCount ?? 0;
+            Comments = model.Comments;
+            AuthorId = model.AuthorId;
+            AuthorName = model.Author.Name;
+            DateOfCreation = model.DateOfCreation;
+            Categories = model.Categories;
+        }
+    }
+}
