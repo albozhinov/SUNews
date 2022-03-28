ae8b85c8bb7c22ca48c32784fc0b2f66fca4a02e
albozhinov <a.bozhinov90@gmail.com>
2022-03-29 00:02:30 +0300

    Delete some properties. Categories property have some bug.

--- SUNews/Models/AllArticlesViewModel.cs
+++ SUNews/Models/AllArticlesViewModel.cs
@@ -6,33 +6,26 @@ namespace SUNews.Models
     {
         public Guid Id { get; init; }
 
-        public string Content { get; init; }
-
         public string Title { get; init; }
 
         public string ImageUrl { get; init; }
 
         public int Likes { get; init; }
 
-        public IEnumerable<Comment> Comments { get; set; } = new List<Comment>();
-
-        public Guid AuthorId { get; init; }
+        public string AuthorName { get; init; }
 
         public DateTime DateOfCreation { get; init; }
 
-        public IEnumerable<ArticleCategory> Categories { get; set; } = new List<ArticleCategory>();
-
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
 
         public AllArticlesViewModel(Article model)
         {
             Id = model.Id;
-            Content = model.Content;
             Title = model.Title;
             ImageUrl = model.ImageUrl;
-            AuthorId = model.AuthorId;
+            AuthorName = model.Author.Name;
             DateOfCreation = model.DateOfCreation;
-            Categories = model.Categories;
-            Comments = model.Comments;
+            //Categories = model.Categories.Select(c => new CategoryViewModel(c.Category)).ToList();
             Likes = model.LikeCount ?? 0;
         }
     }
