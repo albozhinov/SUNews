fecbbe4296044e2467550e3048d94bbe4e439fe3
albozhinov <a.bozhinov90@gmail.com>
2022-03-29 00:03:25 +0300

    CategoryViewModel added. Must to added DTO model in Service layer!

---
+++ SUNews/Models/CategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using SUNews.Data.Models;
+
+namespace SUNews.Models
+{
+    public class CategoryViewModel
+    {
+        public Guid Id { get; init; }
+
+        public string Name { get; init; }
+
+        public ICollection<AllArticlesViewModel> Articles { get; set; } = new List<AllArticlesViewModel>();
+
+        public CategoryViewModel(Category model)
+        {
+            Id = model.Id;
+            Name = model.Name;
+            // Трябва да добавим DTO модели в SUNew.Service за да улесним подаването на данни от
+            // Service layer-a към Web-a !!!
+            Articles = model.Articles.Select(a => new AllArticlesViewModel(a.Article)).ToList();
+        }
+    }
+}
