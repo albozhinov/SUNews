d5ded0cd5ef8fc9d7e4d98821c2a85f77dd3b7c9
albozhinov <a.bozhinov90@gmail.com>
2022-03-30 23:34:04 +0300

    Added servce models to easy data transfer between data-service-web layers.

---
+++ SUNews.Services/Models/CategoryServiceModel.cs
@@ -0,0 +1,22 @@
+﻿using SUNews.Data.Models;
+
+namespace SUNews.Services.Models
+{
+    public class CategoryServiceModel
+    {
+        public Guid Id { get; init; }
+
+        public string Name { get; init; }
+
+        public ICollection<AllArticlesServiceModel> Articles { get; set; } = new List<AllArticlesServiceModel>();
+
+        public CategoryServiceModel(Category model)
+        {
+            Id = model.Id;
+            Name = model.Name;
+            // Трябва да добавим DTO модели в SUNew.Service за да улесним подаването на данни от
+            // Service layer-a към Web-a !!!
+            Articles = model.Articles.Select(a => new AllArticlesServiceModel(a.Article)).ToList();
+        }
+    }
+}
