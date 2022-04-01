8f2123f9f1ca989fc75fc5529b733b7d941c4836
albozhinov <a.bozhinov90@gmail.com>
2022-04-01 22:37:30 +0300

    Add User to default "User" role when new user registered.

--- SUNews/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SUNews/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -121,6 +121,8 @@ namespace SUNews.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, "User");
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
