3818d670ead15e9ad961dd58c3f04d5f6b6053cd
albozhinov <a.bozhinov90@gmail.com>
2022-04-07 01:13:45 +0300

    LikeArticle action added.

--- SUNews/Controllers/ArticleController.cs
+++ SUNews/Controllers/ArticleController.cs
@@ -3,7 +3,9 @@
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
+    using SUNews.Data.Models;
     using SUNews.Models;
+    using SUNews.Providers;
     using SUNews.Services.Constants;
     using SUNews.Services.Contracts;
     using SUNews.Services.Models;
@@ -12,11 +14,15 @@
     {
         private readonly IArticleService articleService;
         private readonly ICategoryService categoryService;
+        private readonly IUserManager<User> userManager;
 
-        public ArticleController(IArticleService _articleService, ICategoryService _categoryService)
+        public ArticleController(IArticleService _articleService, 
+                                 ICategoryService _categoryService,
+                                 IUserManager<User> _userManager)
         {
             this.articleService = _articleService;
             this.categoryService = _categoryService;
+            this.userManager = _userManager;
         }
 
 
@@ -89,6 +95,14 @@
             var article = await articleService.DetailsOfArticleAsync(id);            
 
             return View(article);
+        }
+
+        public async Task<IActionResult> LikeArticle(string id)
+        {
+            var loggedUser = await userManager.GetUserAsync(User);
+            var article = await articleService.LikeArticleAsync(id, loggedUser.Id);
+
+            return RedirectToAction("DetailsOfArticle", new { Id = id });
         }
     }
 }
