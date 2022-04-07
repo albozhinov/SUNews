b628158b12883c0f87cf1081e263811c2a20ee5f
albozhinov <a.bozhinov90@gmail.com>
2022-04-07 19:43:56 +0300

    CreateComment  action added.

--- SUNews/Controllers/CommentController.cs
+++ SUNews/Controllers/CommentController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SUNews.Data.Models;
+using SUNews.Models;
+using SUNews.Providers;
+using SUNews.Services.Contracts;
 
 namespace SUNews.Web.Controllers
 {
     public class CommentController : Controller
     {
-        public IActionResult Index()
+        private readonly ICommentService commentService;
+        private readonly IUserManager<User> userManager;
+
+
+        public CommentController(ICommentService _commentService, IUserManager<User> _userManager)
         {
-            return View();
+            commentService = _commentService;
+            userManager = _userManager;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> CreateComment(CreateCommentViewModel createCommentViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction(createCommentViewModel.CurrentAction, createCommentViewModel.CurrentController, new { id = createCommentViewModel.ArticleId });
+            }
+
+            var user = await userManager.GetUserAsync(User);
+            await commentService.CreateCommentAsync(createCommentViewModel.Text, createCommentViewModel.ArticleId, user.Id);
+
+            return RedirectToAction(createCommentViewModel.CurrentAction, createCommentViewModel.CurrentController, new { id = createCommentViewModel.ArticleId });
         }
     }
 }
