f841aadce8bc5ffabb3fd4ccc0000599f8e8021d
albozhinov <a.bozhinov90@gmail.com>
2022-04-07 19:45:21 +0300

    Added new view model.

---
+++ SUNews/Models/CreateCommentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SUNews.Models
+{
+    public class CreateCommentViewModel
+    {
+        [Required]
+        [StringLength(150, MinimumLength = 6)]
+        public string Text { get; init; }
+
+        [Required]
+        [StringLength(40)]
+        public string ArticleId { get; set; }
+
+        [Required]
+        public string CurrentController { get; set; }
+
+        [Required]
+        public string CurrentAction { get; set; }
+    }
+}
