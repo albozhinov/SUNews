63a67c25dc16529f377022aac3df34df1ef04c3c
albozhinov <a.bozhinov90@gmail.com>
2022-04-07 19:49:40 +0300

    Added new constatns.

--- SUNews.Services/Constants/MessageConstant.cs
+++ SUNews.Services/Constants/MessageConstant.cs
@@ -5,6 +5,14 @@
         public const string ErrorMessage = "ErrorMessage";
         public const string SuccessMessage = "SuccessMessage";
         public const string WarningMessage = "WarningMessage";
+
+        // Article service messages
         public const string ArticleNotFound = "Sorry article not found";
+
+
+        // Comment service messages
+        public const string ErrorUserID = "User with this ID can't found.";
+        public const string ErrorArticleID = "Article with this ID can't found.";
+        public const string ErrorCommentID = "Article with this ID can't found.";
     }
 }
