bfe65da6ef00e840e8eeed967f66efa9303fc0cc
albozhinov <a.bozhinov90@gmail.com>
2022-04-07 19:44:58 +0300

    Added new ViewModel to easy CreateComment in DetailsOfArticle view.

---
+++ SUNews/Models/ArticleAndCommentsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SUNews.Services.Models;
+
+namespace SUNews.Models
+{
+    public class ArticleAndCommentsViewModel
+    {
+        public DetailsOfArticlesServiceModel DetailsOfArticlesServiceModel { get; set; }
+
+        public CreateCommentViewModel CreateCommentViewModel { get; set; }
+    }
+}
