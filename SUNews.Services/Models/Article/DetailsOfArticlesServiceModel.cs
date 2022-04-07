bd9004a9a3d53d578542b3ccba93193d855c3ffe
albozhinov <a.bozhinov90@gmail.com>
2022-04-07 19:52:42 +0300

    Fixed some bugs.

---
+++ SUNews.Services/Models/Article/DetailsOfArticlesServiceModel.cs
@@ -0,0 +1,48 @@
+﻿using SUNews.Data.Models;
+using SUNews.Services.Models.Comment;
+
+namespace SUNews.Services.Models
+{
+    public class DetailsOfArticlesServiceModel
+    {
+        public Guid Id { get; init; }
+
+        public string Content { get; init; }
+
+        public string Title { get; init; }
+
+        public string ImageUrl { get; init; }
+
+        public int Likes { get; init; }
+
+        public IEnumerable<CommentServiceModel> Comments { get; set; } = new List<CommentServiceModel>();
+
+        public Guid AuthorId { get; init; }
+
+        public string AuthorName { get; init; }
+
+        public DateTime DateOfCreation { get; init; }
+
+        public IEnumerable<ArticleCategory> Categories { get; set; } = new List<ArticleCategory>();
+
+
+        public DetailsOfArticlesServiceModel()
+        {
+
+        }
+
+        public DetailsOfArticlesServiceModel(Article model)
+        {
+            Id = model.Id;
+            Content = model.Content;
+            Title = model.Title;
+            ImageUrl = model.ImageUrl;
+            Likes = model.LikeCount ?? 0;
+            Comments = model.Comments.Select(c => new CommentServiceModel(c)).ToList();
+            AuthorId = model.AuthorId;
+            AuthorName = model.Author.Name;
+            DateOfCreation = model.DateOfCreation;
+            Categories = model.Categories;
+        }
+    }
+}
