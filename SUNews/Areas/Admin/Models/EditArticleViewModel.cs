37077e6420a184cb133b9eeeb199f03d08c3849f
albozhinov <a.bozhinov90@gmail.com>
2022-04-09 02:37:24 +0300

    Added CreateArticle, CreateCategory and EditArticle view models.

---
+++ SUNews/Areas/Admin/Models/EditArticleViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+using static SUNews.Data.DataConstants;
+
+namespace SUNews.Areas.Admin.Models
+{
+    public class EditArticleViewModel
+    {
+        [Required]
+        [StringLength(40)]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(ArticleContentMaxLength, MinimumLength = ArticleContentMinLength, ErrorMessage = "Article content must be between {2} and {1}")]
+        public string Content { get; set; }
+
+        [Required]
+        [StringLength(ArticleTitleMaxLength, MinimumLength = ArticleTitleMinLength, ErrorMessage = "Title must be between {2} and {1}")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Image URL")]
+        [StringLength(ArticleImageUrlMaxLength, ErrorMessage = "Image URL must be less than {1} characters.")]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Display(Name = "Author name")]
+        [StringLength(AuthorNameMaxLength, MinimumLength = AuthorNameMinLength, ErrorMessage = "Author name must be between {2} and {1}")]
+        public string AuthorName { get; set; }
+
+        public ICollection<string> Categories { get; set; } = new List<string>();
+
+        // This must to be here to list Categories in View!
+        public IEnumerable<SelectListItem> CategoriesList { get; set; } = new List<SelectListItem>();
+    }
+}
