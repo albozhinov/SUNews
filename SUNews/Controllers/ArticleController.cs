68b9557ef2c655ac77490767becd581dc70bcf2b
albozhinov <a.bozhinov90@gmail.com>
2022-04-09 02:42:52 +0300

    Deleted CreateArticle actions.

--- SUNews/Controllers/ArticleController.cs
+++ SUNews/Controllers/ArticleController.cs
@@ -16,73 +16,23 @@
         private readonly ICategoryService categoryService;
         private readonly IUserManager<User> userManager;
 
-        public ArticleController(IArticleService _articleService, 
+        public ArticleController(IArticleService _articleService,
                                  ICategoryService _categoryService,
                                  IUserManager<User> _userManager)
         {
             this.articleService = _articleService;
-            this.categoryService = _categoryService;
             this.userManager = _userManager;
         }
 
 
         public async Task<IActionResult> Index()
         {
-            var articles = await articleService.GetAllArticlesAsync();           
+            var articles = await articleService.GetAllArticlesAsync();
 
             return View(articles);
-        }
+        }      
 
-        [Authorize(Roles = "Administrator")]
-        public async Task<IActionResult> CreateArticle()
-        {
-            var allCategories = await categoryService.GetAllCategoriesAsync();
-
-            var selectListItems = allCategories
-                                    .Select(c => new SelectListItem { Text = c.Name, })
-                                    .ToList();
-
-            var model = new CreateArticleViewModel()
-            {
-                CategoriesList = selectListItems
-            };
-
-            return View(model);
-        }
-
-        [Authorize(Roles = "Administrator")]
-        [HttpPost]
-        [AutoValidateAntiforgeryToken]
-        public async Task<IActionResult> CreateArticle(CreateArticleViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                ////Here we can return View with invalid data value.
-                ///This we be helpful to write correct value and better user experience.
-                model.CategoriesList = model.Categories.Select(c => new SelectListItem { Text = c }).ToList();
-                return View(model);
-            }
-
-            try
-            {
-                await articleService.CreateArticleAsync(model.Title,
-                                                        model.Content,
-                                                        model.ImageUrl,
-                                                        model.AuthorName,
-                                                        model.Categories);
-
-                return Redirect("/Article/Index");
-            }
-            catch (Exception ex)
-            {
-
-                ViewData[MessageConstant.ErrorMessage] = ex.Message;
-
-                return View("Error");
-            }
-        }
-
-        public  IActionResult AllArticles()
+        public IActionResult AllArticles()
         {
             return RedirectToAction("Index", "Article");
         }
