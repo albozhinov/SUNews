8cd3985380a097948addfdac1263ddeed7b32c48
albozhinov <a.bozhinov90@gmail.com>
2022-04-13 19:03:53 +0300

    Added UserDetaulsViewModel

--- SUNews/Areas/Admin/Models/UserDetailsViewModel.cs
+++ SUNews/Areas/Admin/Models/UserDetailsViewModel.cs
@@ -2,11 +2,15 @@
 {
     using SUNews.Data.Models;
 
-    public class AllUsersViewModel
+    public class UserDetailsViewModel
     {
-        public AllUsersViewModel(User user)
+        public UserDetailsViewModel(User user)
         {
             Id = user.Id;
+            UserName = user.UserName;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            PhoneNumber = user.PhoneNumber;
             Email = user.Email;
             LockoutEnabled = user.LockoutEnabled;
             LockoutEnd = user.LockoutEnd;
@@ -14,19 +18,25 @@
 
             if (String.IsNullOrWhiteSpace(user.FirstName) && String.IsNullOrWhiteSpace(user.LastName))
             {
-                Name = user.Email;
+                UserName = user.Email;
             }
             else
             {
-                Name = user.FirstName + " " + user.LastName;
+                UserName = user.FirstName + " " + user.LastName;
             }
         }
 
         public string Id { get; set; }
 
-        public string Name { get; set; }
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
 
         public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
 
         public string Role { get; set; }
 
