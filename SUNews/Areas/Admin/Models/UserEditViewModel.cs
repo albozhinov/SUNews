0e3b6c65a0c041fbc8dd88d4a98de4f89875d71f
albozhinov <a.bozhinov90@gmail.com>
2022-04-14 00:42:10 +0300

    Added UserIndexViewModel and Edited _UserGrid view.

--- SUNews/Areas/Admin/Models/UserEditViewModel.cs
+++ SUNews/Areas/Admin/Models/UserEditViewModel.cs
@@ -1,20 +1,70 @@
 ﻿namespace SUNews.Areas.Admin.Models
 {
     using Microsoft.AspNetCore.Mvc.Rendering;
+    using SUNews.Data.Models;
     using System.ComponentModel.DataAnnotations;
+
+    using static SUNews.Data.DataConstants;
 
     public class UserEditViewModel
     {
+        public UserEditViewModel()
+        {
+
+        }
+
+        public UserEditViewModel(User user)
+        {
+            Id = user.Id;
+            UserName = user.UserName;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            PhoneNumber = user.PhoneNumber;
+            Email = user.Email;
+            LockoutEnabled = user.LockoutEnabled;
+            LockoutEnd = user.LockoutEnd;
+
+
+            if (String.IsNullOrWhiteSpace(user.FirstName) && String.IsNullOrWhiteSpace(user.LastName))
+            {
+                UserName = user.Email;
+            }
+            else
+            {
+                UserName = user.FirstName + " " + user.LastName;
+            }
+        }
+
+        [Required]
+        [StringLength(IdMaxLength)]
         public string Id { get; set; }
 
-        [Required]
-        [StringLength(50, MinimumLength = 3)]
-        public string FirstName { get; set; }
+        [Display(Name = "Username")]
+        //[StringLength(FirstAndLastNameLength)]
+        public string? UserName { get; set; }
 
-        [Required]
-        [StringLength(50, MinimumLength = 3)]
-        public string LastName { get; set; }
+        //[Required]
+        [Display(Name = "First name")]
+        //[StringLength(FirstAndLastNameLength, MinimumLength = 3)]
+        public string? FirstName { get; set; }
 
+        //[Required]
+        [Display(Name = "Last name")]
+        //[StringLength(FirstAndLastNameLength, MinimumLength = 3)]
+        public string? LastName { get; set; }
+
+        [Display(Name = "Email address")]
+        public string? Email { get; set; }
+
+        [Display(Name = "Phone number")]
+        //[RegularExpression("^[0-9]", ErrorMessage = "Please enter only digits between 0 - 9.")]
+        public string? PhoneNumber { get; set; }
+
+        public bool? LockoutEnabled { get; set; }
+
+        public DateTimeOffset? LockoutEnd { get; set; }
+
+        [Display(Name = "User roles")]
         public ICollection<string> Roles { get; set; } = new List<string>();
 
         // This must to be here to list Categories in View!
---
+++ SUNews/Areas/Admin/Models/UserIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SUNews.Areas.Admin.Models
+{
+    using SUNews.Data.Models;
+    using X.PagedList;
+
+    public class UserIndexViewModel
+    {
+        public UserIndexViewModel(IEnumerable<User> users, int pageNumber, int pageSize)
+        {
+            this.Users = users.Select(u => new AllUsersViewModel(u)).ToPagedList(pageNumber, pageSize);
+        }
+
+        public string StatusMessage { get; set; }
+
+        public IPagedList<AllUsersViewModel> Users { get; set; }
+    }
+}
