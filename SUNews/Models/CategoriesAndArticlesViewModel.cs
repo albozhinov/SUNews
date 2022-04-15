71bed5e1e3833e4e0efdec4bfc6c6223bb8d8614
albozhinov <a.bozhinov90@gmail.com>
2022-04-16 00:20:01 +0300

    Added new view model

---
+++ SUNews/Models/CategoriesAndArticlesViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SUNews.Models
+{
+    using SUNews.Services.Models;
+
+    public class CategoriesAndArticlesViewModel
+    {
+        public string ChoosenCategory { get; set; }
+
+        public IEnumerable<CategoryServiceModel> Categories { get; set; }
+
+        public IEnumerable<ArticleServiceModel> Articles { get; set; }
+    }
+}
