81125c0d56a1bae77ebec380664be50461cf0445
albozhinov <a.bozhinov90@gmail.com>
2022-04-16 00:23:42 +0300

    Added AllCategories  and GetArticleByCAtegory methods.

--- SUNews.Services/Services/CategoryService.cs
+++ SUNews.Services/Services/CategoryService.cs
@@ -8,6 +8,8 @@
     using SUNews.Services.Models;
     using SUNews.Services.Providers;
 
+    using static SUNews.Services.Constants.MessageConstant;
+
     public class CategoryService : ICategoryService
     {
         private readonly IRepository repository;
@@ -17,6 +19,20 @@
         {
             repository = _repository;
             validator = validatorSerivce;
+        }
+
+        public async Task<IEnumerable<CategoryServiceModel>> AllCategories()
+        {
+            return await repository.All<Category>()
+                                   .Include(c => c.Articles)
+                                   .Select(c => new CategoryServiceModel() 
+                                   { 
+                                       Id = c.Id,
+                                       Name = c.Name,
+                                       ArticlesCount = c.Articles.Count
+                                   })
+                                   .ToListAsync();
+
         }
 
         public async Task<string> CreateCategoryAsync(string categoryName)
@@ -44,6 +60,7 @@
             return categoryToAdd.Name;
         }
 
+        // TODO: This method must be edit! 
         public async Task<IEnumerable<CategoryServiceModel>> GetAllCategoriesAsync()
         {
             var allDbCategories = await repository.All<Category>()
@@ -57,5 +74,52 @@
 
             return categoriesSM;
         }
+
+        public async Task<IEnumerable<ArticleServiceModel>> GetArticlesByCategory(string id)
+        {
+            validator.NullOrWhiteSpacesCheck(id);
+
+            (bool, Guid) isValidId = validator.TryParseGuid(id);
+
+            if (!isValidId.Item1)
+                throw new ArgumentNullException(CategoryNotFound);
+
+            var articlesByCategory = await repository.All<ArticleCategory>()
+                                                     .Where(ac => ac.CategoryId == isValidId.Item2 && !ac.Article.IsDeleted)
+                                                     .Select(ac => new ArticleServiceModel()
+                                                     {
+                                                         Id = ac.ArticleId.ToString(),
+                                                         Title = ac.Article.Title,
+                                                         AuthorName = ac.Article.Author.Name,
+                                                         Likes = ac.Article.LikeCount,
+                                                         CommentsCount = ac.Article.Comments.Count
+                                                     })
+                                                     .ToListAsync();
+
+
+
+                                                        //.Where(c => c.Id == isValidId.Item2)
+                                                        //.Include(c => c.Articles)
+                                                        //    .ThenInclude(ac => ac.Article)
+                                                        //    .ThenInclude(a => a.Comments)
+                                                        //.Include(c => c.Articles)
+                                                        //    .ThenInclude(ac => ac.Article)
+                                                        //    .ThenInclude(a => a.Author)
+                                                        //.Select(c => c.Articles.Select(a => new ArticleServiceModel()
+                                                        //{
+                                                        //    Id = a.ArticleId.ToString(),
+                                                        //    AuthorName = a.Article.Author.Name,
+                                                        //    Title = a.Article.Title,
+                                                        //    Likes = a.Article.LikeCount ?? 0,
+                                                        //    CommentsCount = a.Article.Comments.Count,
+                                                        //}))
+                                                        //.FirstOrDefaultAsync();
+
+            if (articlesByCategory is null)
+                throw new ArgumentNullException(CategoryNotFound);
+
+
+            return articlesByCategory;
+        }        
     }
 }
