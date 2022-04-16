17c8349ee212a0af5180392d57767ad040fd70f7
albozhinov <a.bozhinov90@gmail.com>
2022-04-16 14:23:02 +0300

    Deleted SUNews.Tests

---
+++ SUNews.Tests/Services/ArticleServiceTests/CreateArticleShould.cs
@@ -0,0 +1,43 @@
+﻿namespace SUNews.Tests.Services.ArticleServiceTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using SUNews.Data.Context;
+    using SUNews.Data.Repository;
+    using SUNews.Services.Providers;
+    using SUNews.Services.Services;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class CreateArticleShould
+    {
+        [DataTestMethod]
+        [DataRow(null, "Valid Content", "Valid Url", "Valid AuthorName")]
+        public async Task ThrowArgumentException_WhenArgumentsAreNull(string title,
+                                                                            string content,
+                                                                            string imageUrl,
+                                                                            string authorName)
+        {
+            // Arrange
+            var dbContextStub = new Mock<IRepository>();
+            var validatorServiceProviderMock = new Mock<IValidatorService>();
+
+            var sut = new ArticleService(dbContextStub.Object, validatorServiceProviderMock.Object);
+
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await sut.CreateArticleAsync(title,
+                                                                                                              content,
+                                                                                                              imageUrl,
+                                                                                                              authorName,
+                                                                                                              new List<string>()));
+        }
+
+
+
+    }
+
+}
